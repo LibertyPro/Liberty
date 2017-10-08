feb9cf5d4a609b72f644b0fc823191e47f1db3c7
emanzelekha <emanzelekha@gmail.com>
2017-10-08 14:58:41 +0200

    as

--- app/src/main/java/com/example/elashry/liberty/MainActivity.java
+++ app/src/main/java/com/example/elashry/liberty/MainActivity.java
@@ -3,11 +3,74 @@ package com.example.elashry.liberty;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity  implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
+    private SliderLayout mDemoSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDemoSlider = (SliderLayout) findViewById(R.id.slider);
+
+
+        HashMap<String, Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("كاميرات مراقبه", R.drawable.img1);
+        file_maps.put("تطبيقات موبيل ", R.drawable.img2);
+        file_maps.put("كاميرا ثلاثيه", R.drawable.img3);
+        file_maps.put("تسويق منتجات", R.drawable.img4);
+        file_maps.put("تصميم لوجهات واعلانات", R.drawable.img1);
+        file_maps.put("تصميم مواقع", R.drawable.img2);
+        file_maps.put("تصميم بوسترات ومنيوهات", R.drawable.img3);
+
+
+        for (String name : file_maps.keySet()) {
+            TextSliderView textSliderView = new TextSliderView(this);
+            // initialize a SliderLayout
+            textSliderView.description(name)
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle().putString("extra", name);
+            mDemoSlider.addSlider(textSliderView);
+        }
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(2000);
+        mDemoSlider.addOnPageChangeListener(this);
+
+
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
